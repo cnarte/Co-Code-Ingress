34892f0b32f073753d51b3dd6e52d44f94b24f42
Ankit Kumar <ankitkrchy.54@gmail.com>
2021-02-13 15:32:48 +0530

    1st

---
+++ client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 22:59:21 2018
+
+@author: devops
+"""
+# Importing the libraries
+from os import link
+import time
+import requests
+import json
+
+target = "http://127.0.0.1:3091/"
+urls = target + '/api/fight/'
+ws = open('/home/cnarte/Ankit/ingress/Co-Code-Ingress/Real-Time-Violence-Detection-in-Video-/hdfight.mp4', 'rb')#.read()
+link = 'http://721585b6ef79.ngrok.io/video_feed'
+files = {'file': ws , "link" : link }
+info = {'gps':'12344333','id':'11977354',"time-stamp":'3240234049820349'}
+millis = int(round(time.time() * 1000))
+r = requests.post(urls, data=info, files=files)
+json_data = json.loads(r.text)
+millis2 = int(round(time.time() * 1000))
+print("processing time in server " ,json_data['processing_time'])
+print("tootal time from client and all is  " ,str(millis2-millis))
+print(json_data['precentegeoffight'])
+#if float(json_data['precentegeoffight']) >= 0.78:
+#    reqs = requests.get('https://mamonrasool.com/mastermamon/pushmamon.php?USBBAN10388=azm00774615nEeaoknva110205678&mapaaSSS13455znb0=Azncb87268$$22mmsknnns0199556782&MSG='+msgg)
